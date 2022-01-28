15b278e9b00bbe54f2a55ffaa66cadfa2703ac61
Elliot Iddon <elliot@land.tech>
2022-01-28 12:07:10 +0000

    Fri 28 Jan 2022 12:07:09 GMT

--- check.py
+++ check.py
@@ -1,5 +1,39 @@
 # Create a wordle validator
+# YNMMN
+# Y - This is the right letter in the right place
+# M - This is the right letter but the wrong place
+# N - Ths is the wrong letter
 
+WORD_LENGTH = 5
 
-def validate(guess: str, target: str):
+class NotFiveLetterError(Exception):
     pass
+
+class InvalidWordError(Exception):
+    pass
+
+def validate(guess: str, target: str) -> str:
+    if len(guess) != WORD_LENGTH:
+        raise NotFiveLetterError(f"whoopsie you've supplied a {len(guess)} word")
+    if len(target) != WORD_LENGTH:
+        raise NotFiveLetterError(f"oops, target {target} is too short: {len(target)}")
+    
+    exp_target = list(target)
+    result = ["N"] * WORD_LENGTH
+    for i, (a, b) in enumerate(zip(guess, exp_target)):
+        if a == b:
+            result[i] = "Y"
+            exp_target[i] = None
+
+    for i, (a, r) in enumerate(zip(guess, result)):
+        if r == "Y":
+            continue
+        try:
+            exp_i = exp_target.index(a)
+        except ValueError:
+            pass  # guessed letter is not in word at all
+        else:
+            result[i] = "M"
+            exp_target[exp_i] = None
+
+    return "".join(result)
--- test_check.py
+++ test_check.py
@@ -1,5 +1,29 @@
 import pytest
 import check
 
-def test_boring():
-    check.validate("ABCDEF", "WORDS")
+
+def test_all_wrong():
+    assert check.validate("ABCDE", "WORMS") == "NNNNN"
+
+
+def test_output_matches_input_length():
+    assert len(check.validate("ABCDE", "WORDS")) == check.WORD_LENGTH
+
+@pytest.mark.parametrize("guess, target", (("ADGF", "WORDS"), ("ABCDE", "WORD"), ("ADAGES", "WORDS"), ("SAND", "PIER")))
+def test_wrong_length_raises_error(guess, target):
+    with pytest.raises(check.NotFiveLetterError):
+        check.validate(guess, target)
+
+def test_all_right():
+    assert check.validate("WORDS", "WORDS")== "YYYYY"
+
+def test_right_letter_wrong_place():
+    assert check.validate("SWORD", "WORDS") == "MMMMM"
+
+@pytest.mark.parametrize("guess, target, expected", (
+    ("SABRE", "CAPER", "NYNMM"),
+    ("MUMMY", "YUMMY", "NYYYY"),
+    ("CHILL", "LEMON", "NNNMN"),
+))
+def test_exercise_ymn_cases(guess, target, expected):
+    assert check.validate(guess, target) == expected
